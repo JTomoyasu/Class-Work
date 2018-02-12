4d3b390596f4c024ff2ce06381c404dafc5a6aab
JTomoyasu <30738319+JTomoyasu@users.noreply.github.com>
2018-02-12 12:34:22 -0800

    done

--- stairs.js
+++ stairs.js
@@ -1,5 +1,17 @@
-+" _"
-for(i=0;i<n;i++)
-{
-    
+function stairs(n) {
+    var str = "";
+    for (j = 0; j < n; j++) {
+        str = "  " + str;
+        //spaces accumulate 
+    }
+    for (i = 0; i < n; i++) {
+        /* for (j = i; j < (n - 1); j++) {
+            str = "  " + str;
+            //spaces accumulate 
+        } */
+        str=str.substring(2);
+        str += " _";
+        console.log(str);
+    }
 }
+stairs(20);
